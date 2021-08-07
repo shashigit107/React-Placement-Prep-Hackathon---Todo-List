67ef36fc21dc972c29690234dd965c337bb73ecc
shashigit107 <83776652+shashigit107@users.noreply.github.com>
2021-08-08 00:35:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,66 @@
-import React from "react";
-import "./../styles/App.css";
+import { useState } from 'react';
+import './App.css';
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const[input,setInput]=useState("");
+  const[quarterfinal,setQuarterfinal]=useState([]);
+  const[editingid,setEditingId]=useState(null);
+  const[editingValue,setEditingValue]=useState("");
+ 
+  const addHandler=()=>{
+    const newVariable={
+      id:new Date().getTime(),
+      text: input
+    } 
+    setQuarterfinal([...quarterfinal, newVariable]);
+    console.log(newVariable)
+    setInput("");
+  }
+  const deleteHandler=(id)=>{
+    const upadte=[...quarterfinal].filter((para)=>{
+      return para.id!==id;
+    })
+    setQuarterfinal(upadte);
+  }
+  const editSubmitHandler=(id)=>{
+    const updatevalue=[...quarterfinal].map((para)=>{
+      if(para.id===id){
+        para.text=editingValue;
+      }
+      return para;
+    })
+    //console.log(updatevalue)
+    setQuarterfinal(updatevalue);
+    console.log(quarterfinal)
+    setEditingId(null);
+  }
+  return (
+    <div className="App">
+      <input id="task" value={input} onChange={(e)=>setInput(e.target.value)}></input>
+     <button id="btn" onClick={addHandler}> Add </button>
+     {/* {console.log("return")} */}
+
+     {quarterfinal.map((para)=>{
+       return (
+         <>
+         
+         <div key={para.id}>
+           {editingid===para.id
+           ?(<input id="task" onChange={(e)=>setEditingValue(e.target.value)}/> )
+           :(<li className="list">{para.text}</li>)}
+            {editingid===para.id
+            ?(<button type="submit" onClick={()=>editSubmitHandler(para.id)}>EDIT SUBMIT</button>)
+          :(<button className="edit" onClick={()=>setEditingId(para.id)}> EDIT </button>)}
+            
+            <button className="delete" onClick={()=>deleteHandler(para.id)}> DELETE </button>
+            
+
+         </div>
+         </>
+       )
+     })}
+    </div>
+  );
 }
-
 
 export default App;
